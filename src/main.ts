98fca86a414fda8f363c560d8ae580a0bc8d8e6d
Nemmy`s Laptop <kuram@DESKTOP-5GISHRQ>
2023-07-13 22:52:56 +0900

    Add Shader.wgsl

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-import { CheckWebGPU } from "./helper"
+import { CheckWebGPU } from "./helper";
+import { shader } from './shader.wgsl';
+import "./site.css";
+
+const CreateTriangle = async () =>{
+    const checkgpu = CheckWebGPU();
+    if( checkgpu.includes('Your current Browser does not support WebGPU!')){
+        console.log(checkgpu);
+        throw('Your current Browser does not support WebGPU!');
+    }
+
+    const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;
+    const adapter = await navigator.gpu?.requestAdapter() as GPUAdapter;
+    const device = await adapter?.requestDevice() as GPUDevice;
+    const context = canvas.getContext('webgpu') as GPUCanvasContext;
+    const format = navigator.gpu?.getPreferredCanvasFormat();
+    context.configure({
+        device: device,
+        format: format,
+    });
+}
